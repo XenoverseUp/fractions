75f568721428d2b2c0ce508c87e99daae96e40e8
XenoverseUp <xenoverseup@gmail.com>
2022-03-20 01:13:51 +0300

    background script added

--- src/backend/background.js
+++ src/backend/background.js
@@ -1,1 +1,12 @@
-console.log("running babehh");
+getEarningData().then(res => console.log(res));
+
+async function getEarningData() {
+	const res = await fetch(
+		"https://medium.com/me/partner/dashboard?format=json"
+	);
+	const text = await res.text();
+	const validJson = text.split("</x>")[1];
+	const data = await JSON.parse(validJson);
+
+	return data;
+}

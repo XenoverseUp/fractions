1fba7f114752c48e7a2d30056ce23a1c98f8011c
XenoverseUp <xenoverseup@gmail.com>
2022-04-16 02:55:13 +0300

    Version across codebase is binded to manifest version.

--- scripts/set_version.py
+++ scripts/set_version.py
@@ -14,10 +14,16 @@ with open(
     manifest = json.load(data)
     version = manifest["version"]
 
-# TODO Check if there is already a VERSION in env file.
 with open(
     os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), "../.env")),
-    "a",
+    "r+",
 ) as env:
-    env.write("\n")
-    env.write(f"VERSION={version}")
+    lines = []
+    for line in env.readlines():
+        if (not line.startswith("VERSION")) and line != "\n":
+            lines.append(line)
+
+    lines.append(f"VERSION={version}")
+    env.seek(0)
+    env.truncate(1)
+    env.write("\n".join(lines))

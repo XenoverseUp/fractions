e172ffe35caec56e666430ad3daabc1017942858
XenoverseUp <xenoverseup@gmail.com>
2022-04-16 11:49:21 +0300

    New field are added to manifests.

--- scripts/update_manifest.py
+++ scripts/update_manifest.py
@@ -4,6 +4,18 @@ import os
 import argparse
 import json
 
+parser = argparse.ArgumentParser(
+    description="This will update the manifests of the build."
+)
+
+parser.add_argument(
+    "build", choices=["chromium", "firefox"], help="The target browser."
+)
+args = parser.parse_args()
+
+root = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__), f"../build/{args.build}")
+)
 
 appropriate_manifest = {}
 
@@ -21,46 +33,24 @@ with open(
     appropriate_manifest["author"] = manifest["author"]
     appropriate_manifest["icons"] = manifest["icons"]
 
-parser = argparse.ArgumentParser(
-    description="This will update the manifests of the build."
-)
-
-parser.add_argument(
-    "build", choices=["chromium", "firefox"], help="The target browser."
-)
-args = parser.parse_args()
-
-
-root = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__), f"../build/{args.build}")
-)
-
-if args.build == "chromium":
-    appropriate_manifest["manifest_version"] = 3
-    appropriate_manifest["host_permissions"] = ["https://medium.com/"]
-    appropriate_manifest["background"] = {"service_worker": "/serviceWorker.js"}
-    appropriate_manifest["action"] = {
-        "default_title": "Fractions",
-        "default_popup": "/popup.html",
-    }
-elif args.build == "firefox":
-    appropriate_manifest["manifest_version"] = 2
-    appropriate_manifest["short_name"] = "Fractions"
-    appropriate_manifest["permissions"] = ["https://medium.com/"]
-    appropriate_manifest["background"] = {
-        "scripts": ["serviceWorker.js"],
-        "persistent": False,
-    }
-    appropriate_manifest["browser_action"] = {
-        "default_title": "Fractions",
-        "default_popup": "/popup.html",
-    }
-    appropriate_manifest["browser_specific_settings"] = {
-        "gecko": {
-            "id": "{6a3d8bbc-24ac-4d2c-8b67-c658388e9c8d}",
-            "strict_min_version": "57.0",
+    if args.build == "chromium":
+        appropriate_manifest["manifest_version"] = manifest["chromium"][
+            "manifest_version"
+        ]
+        appropriate_manifest["host_permissions"] = manifest["permissions"]
+        appropriate_manifest["background"] = {
+            "service_worker": manifest["background"]["scripts"][0]
         }
-    }
+        appropriate_manifest["action"] = manifest["browser_action"]
+    elif args.build == "firefox":
+        appropriate_manifest["manifest_version"] = manifest["manifest_version"]
+        appropriate_manifest["short_name"] = manifest["firefox"]["short_name"]
+        appropriate_manifest["permissions"] = manifest["permissions"]
+        appropriate_manifest["background"] = manifest["background"]
+        appropriate_manifest["action"] = manifest["browser_action"]
+        appropriate_manifest["browser_specific_settings"] = manifest["firefox"][
+            "browser_specific_setttings"
+        ]
 
 with open(os.path.join(root, "manifest.json"), "w") as manifest:
     manifest.write(json.dumps(appropriate_manifest))

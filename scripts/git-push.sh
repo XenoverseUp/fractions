e1b15a5801c47f7c2725bc2895f86d50c50533ef
XenoverseUp <xenoverseup@gmail.com>
2022-04-24 00:47:52 +0300

    Force option is added to push script.

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -6,4 +6,9 @@ echo "Enter the commit message: "
 read COMMIT_MESSAGE
 
 git commit -m "${COMMIT_MESSAGE}"
-git push -u origin main
+
+if [ "$1" == "force"]; then 
+	git push -f origin main
+else
+	git push -u origin main
+fi
